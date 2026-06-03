1ea43daff3c1c7c88572826398416a881068faff
pdp7093-brainstream <deepak.prasad@brainstream.tech>
2026-06-03 18:09:08 +0530

    Update settings.py

--- doctor_mgmt/settings.py
+++ doctor_mgmt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=0+5%4&0&1n8@h*py+hpmamjym3kqqnnu*+m)^z4k*&54p$bwh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
